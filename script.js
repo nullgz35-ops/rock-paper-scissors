be5e6e08ec344d3f81e27dc624101ce2f1ad5d2b
archie <archiecharisma@gmai.com>
2026-08-03 17:09:58 -0400

    feat:add ui images and event listeners

--- script.js
+++ script.js
@@ -1,52 +1,64 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerChoice () {    //pilihan 
-    let randomNumber = Math.floor (Math.random() * 3);
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
     let computerChoice;
-    if (randomNumber === 0){
-        computerChoice = "batu"
-    } else if (randomNumber === 1){
-        computerChoice = "kertas"
+    if (randomNumber === 0) {
+        computerChoice = "batu";
+    } else if (randomNumber === 1) {
+        computerChoice = "kertas";
     } else {
-        computerChoice = "gunting"
+        computerChoice = "gunting";
     }
-    return computerChoice 
+    return computerChoice;
 }
 
-function getHumanChoice() {
-    return prompt ("Pilih batu, gunting, atau kertas: ").toLowerCase();
-}
-
-function playRound (humanChoice, computerChoice) {   //penentu
-    humanChoice = humanChoice.toLowerCase ();
-   if (humanChoice === "batu" && computerChoice === "gunting"){ humanScore++;
-        return humanChoice + " vs " + computerChoice + ' menang'
-    } else if (humanChoice === "gunting" && computerChoice === "kertas"){humanScore++;
-        return humanChoice + " vs " + computerChoice + " menang"
-    } else if (humanChoice === "kertas" && computerChoice === "batu"){humanScore++;
-        return humanChoice + " vs " + computerChoice + ' menang'
-    } else if (humanChoice === computerChoice){
-        return humanChoice + " vs " + computerChoice + " seri"
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "batu" && computerChoice === "gunting") {
+        humanScore++;
+        return "Player menang!";
+    } else if (humanChoice === "gunting" && computerChoice === "kertas") {
+        humanScore++;
+        return "Player menang!";
+    } else if (humanChoice === "kertas" && computerChoice === "batu") {
+        humanScore++;
+        return "Player menang!";
+    } else if (humanChoice === computerChoice) {
+        return "Seri!";
     } else {
         computerScore++;
-        return humanChoice + " vs " + computerChoice + " kalah"
+        return "Komputer menang!";
     }
 }
 
-function playGame (){ 
-    for (let i=0; i<5; i++){
-        let humanChoice = getHumanChoice ()
-        let computerChoice = getComputerChoice ()
-        let hasil = playRound(humanChoice, computerChoice)
-        console.log (hasil)
-    }
-    if (humanScore> computerScore){
-        console.log ("player menang!")
-    } else if (humanScore < computerScore){
-        console.log ("Player kalah!")
-    } else {
-        console.log ("Seri!")
-    } 
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const resultDisplay = document.querySelector("#result");
+const scoreDisplay = document.querySelector("#score");
+const computerImg = document.querySelector("#computer-img");
+const playerImg = document.querySelector("#player-img");
+
+function handleChoice(humanChoice) {
+    const computerChoice = getComputerChoice();
+    const result = playRound(humanChoice, computerChoice);
+
+    playerImg.src = humanChoice + ".png";
+    computerImg.src = computerChoice + ".png";
+    resultDisplay.textContent = result;
+    scoreDisplay.textContent = "Your score: " + humanScore;
 }
-playGame ();
+
+rockBtn.addEventListener("click", function() {
+    handleChoice("batu");
+});
+
+paperBtn.addEventListener("click", function() {
+    handleChoice("kertas");
+});
+
+scissorsBtn.addEventListener("click", function() {
+    handleChoice("gunting");
+});
